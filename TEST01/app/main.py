5a64baf299da233a42a1ba19bb499975c7e47809
CJH <thinkbox3064@gmail.com>
2022-10-07 16:49:15 +0900

    second

--- TEST01/app/main.py
+++ TEST01/app/main.py
@@ -9,8 +9,15 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_main(value):
+    # comment
+    print('main program start')
+    print(f'{value}')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_main('main')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
